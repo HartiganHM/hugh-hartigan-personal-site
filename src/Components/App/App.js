e300d645691b5285e2855bd015de24e49cc32b9c
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-18 16:27:48 -0700

    Add Links to other Components

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        I am an App
+        <Link to='/about'>About</Link>
+        <Link to='/projects'>Projects</Link>
+        <Link to='/links'>Links</Link>
+        <Link to='/blogs'>Blogs</Link>
       </div>
     );
   }
