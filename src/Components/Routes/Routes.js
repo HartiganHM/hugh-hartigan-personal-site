4f72afbf391d3a9e6b16656f07884104b3b6eab4
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-18 15:45:34 -0700

    Build out anticipated Routes and remove unneeded css and scss files

--- src/Components/Routes/Routes.js
+++ src/Components/Routes/Routes.js
@@ -1,14 +1,21 @@
-import React, { Component } from 'react';
-import './Routes.css';
+import React from 'react';
+import { Route } from 'react-router-dom';
+import App from '../App/App';
+import About from '../About/About';
+import Links from '../Links/Links';
+import Projects from '../Projects/Projects';
+import Blogs from '../Blogs/Blogs';
 
-class Routes extends Component {
-  render() {
-    return (
-      <div className="Routes">
-        I am an Routes
-      </div>
-    );
-  }
-}
+const Routes = () => {
+  return (
+    <div className="Routes">
+      <Route exact to='/' component={App} />
+      <Route to='/about' componet={About} />
+      <Route to='/links' componet={Links} />
+      <Route to='/projects' componet={Projects} />
+      <Route to='/blogs' componet={Blogs} />
+    </div>
+  );
+};
 
 export default Routes;
