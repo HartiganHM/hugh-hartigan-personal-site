49bae49ee03f60d3e6fd45be8f23ecedfcb9f42e
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-18 16:39:46 -0700

    Remove links from App and render Routes only

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import Routes from '../Routes/Routes';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Link to='/about'>About</Link>
-        <Link to='/projects'>Projects</Link>
-        <Link to='/links'>Links</Link>
-        <Link to='/blogs'>Blogs</Link>
+        <Routes />
       </div>
     );
   }
