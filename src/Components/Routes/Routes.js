5bd707d49c75cd1d555f975b9ca09d74b0f9249b
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-18 15:41:09 -0700

    Mock out all components

---
+++ src/Components/Routes/Routes.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Routes.css';
+
+class Routes extends Component {
+  render() {
+    return (
+      <div className="Routes">
+        I am an Routes
+      </div>
+    );
+  }
+}
+
+export default Routes;
