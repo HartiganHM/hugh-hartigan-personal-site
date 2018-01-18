a2fd2799cdfee8ffdeec5172526a2fb4fc9ebb26
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-18 16:43:37 -0700

    Render LandingPage on root path instead of App

--- src/Components/Routes/Routes.js
+++ src/Components/Routes/Routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import App from '../App/App';
+import LandingPage from '../LandingPage/LandingPage';
 import About from '../About/About';
 import Links from '../Links/Links';
 import Projects from '../Projects/Projects';
@@ -9,7 +9,7 @@ import Blogs from '../Blogs/Blogs';
 const Routes = () => {
   return (
     <div>
-      <Route exact path='/' component={App} />
+      <Route exact path='/' component={LandingPage} />
       <Route path='/about' componet={About} />
       <Route path='/links' componet={Links} />
       <Route path='/projects' componet={Projects} />
