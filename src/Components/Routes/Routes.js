1c3ba5b43848c0ec04be8b9d48703d6ae0412ec5
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-19 09:47:05 -0700

    Add TransitionGroup and CSSTransition to Routes

--- src/Components/Routes/Routes.js
+++ src/Components/Routes/Routes.js
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import LandingPage from '../LandingPage/LandingPage';
 import About from '../About/About';
 import Links from '../Links/Links';
 import Projects from '../Projects/Projects';
 import Blogs from '../Blogs/Blogs';
+import './Routes.css';
 
-const Routes = () => {
-  return (
-    <div>
-      <Switch>
-        <Route exact path="/about" render={() => <About />} />
-        <Route exact path="/projects" render={() => <Projects />} />
-        <Route exact path="/links" render={() => <Links />} />
-        <Route exact path="/blogs" render={() => <Blogs />} />
-        <Route exact path="/" component={LandingPage} />
-      </Switch>
-    </div>
-  );
+class Routes extends Component {
+  render() {
+    return (
+      <TransitionGroup className='transition-group'>
+        <CSSTransition
+          key={this.props.location.key}
+          classNames="fade"
+          timeout={300}
+          mountOnEnter={true}
+          unmountOnExit={true}
+        >
+          <div className="switch-wrapper">
+            <Switch location={this.props.location}>
+              <Route exact path="/about" render={() => <About />} />
+              <Route exact path="/projects" render={() => <Projects />} />
+              <Route exact path="/links" render={() => <Links />} />
+              <Route exact path="/blogs" render={() => <Blogs />} />
+              <Route exact path="/" render={() => <LandingPage />} />
+            </Switch>
+          </div>
+        </CSSTransition>
+      </TransitionGroup>
+    );
+  }
 };
 
 export default Routes;
