f6f4baf9965ff0398043e3d9451b2eb24e11ce01
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-19 09:46:07 -0700

    Import Route to pass location to Routes

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Routes from '../Routes/Routes';
+import { Route } from 'react-router';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-      <Routes />
+      <Route to="/" component={Routes} />
     </div>
   );
 };
