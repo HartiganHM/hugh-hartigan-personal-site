ededfc3e9ed48440bc8ec1f1f8ed94efcfedf66b
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-18 20:59:47 -0700

    Build firstChild const, import TransitionGroup, and Switch

--- src/Components/Routes/Routes.js
+++ src/Components/Routes/Routes.js
@@ -1,19 +1,68 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import TransitionGroup from 'react-transition-group/TransitionGroup';
 import LandingPage from '../LandingPage/LandingPage';
 import About from '../About/About';
 import Links from '../Links/Links';
 import Projects from '../Projects/Projects';
 import Blogs from '../Blogs/Blogs';
 
+const firstChild = props => {
+  const childrenArray = React.Children.toArray(props.children);
+
+  return childrenArray[0] || null;
+};
+
 const Routes = () => {
   return (
     <div>
-      <Route exact path='/' component={LandingPage} />
-      <Route path='/about' render={() => <About />} />
-      <Route path='/links' render={() => <Links />} />
-      <Route path='/projects' render={() => <Projects />} />
-      <Route path='/blogs' render={() => <Blogs />} />
+      <Switch>
+        <Route
+          exact
+          path="/about"
+          children={({ match, ...rest }) => (
+            <TransitionGroup component={firstChild}>
+              {match && <About {...rest} />}
+            </TransitionGroup>
+          )}
+        />
+        <Route
+          exact
+          path="/links"
+          children={({ match, ...rest }) => (
+            <TransitionGroup component={firstChild}>
+              {match && <Links {...rest} />}
+            </TransitionGroup>
+          )}
+        />
+        <Route
+          exact
+          path="/projects"
+          children={({ match, ...rest }) => (
+            <TransitionGroup component={firstChild}>
+              {match && <Projects {...rest} />}
+            </TransitionGroup>
+          )}
+        />
+        <Route
+          exact
+          path="/blogs"
+          children={({ match, ...rest }) => (
+            <TransitionGroup component={firstChild}>
+              {match && <Blogs {...rest} />}
+            </TransitionGroup>
+          )}
+        />
+        <Route
+          exact
+          path="/"
+          children={({ match, ...rest }) => (
+            <TransitionGroup component={firstChild}>
+              {match && <LandingPage {...rest} />}
+            </TransitionGroup>
+          )}
+        />
+      </Switch>
     </div>
   );
 };
