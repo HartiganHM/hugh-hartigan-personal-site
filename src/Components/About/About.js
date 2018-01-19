f41a29e53a6062292301872f165c31a0133910cf
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-19 09:44:31 -0700

    Add link to Home

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './About.css';
 
 const About = () => {
-  return <div className="About">I am an About</div>;
+  return (
+    <div className="About">
+      I am an About
+      <Link className='nav-link' to="/">Home</Link>
+    </div>
+  );
 };
 
 export default About;
