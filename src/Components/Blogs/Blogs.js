0d677ba361dd0e0a7baffb750ce26a3813ebdf3f
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-19 17:09:43 -0700

    Build blogPost map and import Card component

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Card from '../Card/Card';
+import blogsData from '../../data/blogsData';
 import './Blogs.css';
 
 class Blogs extends Component {
   render() {
-    
+    const blogPosts = blogsData.map(blog => <Card data={blog}/>)
     return (
       <div className="Blogs">
         <Link className="nav-link nav-link-right" to="/">
