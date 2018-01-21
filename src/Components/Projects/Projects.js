27f125015ffc574a59f5b887f5d77b4c21af22fe
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-21 13:50:27 -0700

    Add header to Projects component and style

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './Projects.css';
 
 class Projects extends Component {
   render() {
     return (
       <div className="Projects">
-        I am an Projects
+        <div className="wrapper">
+          <span className="header-wordmark" />
+          <Link className="nav-link nav-link-left" to="/">
+            Home
+          </Link>
+        </div>
       </div>
     );
   }
