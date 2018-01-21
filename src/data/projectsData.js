492393dc829fcd78d4a6942a04f4bf774f0a5654
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-21 16:44:00 -0700

    Extend projectsData object

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,9 +1,24 @@
 export default [
   {
+    image: 'image our-planet-screen-shot',
     title: 'Our Planet',
-    description: 'A study in React, Redux, and Node.js centered around the well being of endangered animals across the globe',
+    description: 'Our Planet is a web application built as part of the curriculum at Turing School of Software & Design. The project incorporates both a back-end database and a front-end user interface. The back-end repository was built using Node.js and Knex, while the front-end was constructed primarily through React and Redux. All styling was done through Sass and the testing suite runs through Enzyme and Jest.',
     techStack: 'React, Redux, Router, Node.js, Knex, PostgreSQL',
     gitHub: 'https://github.com/HartiganHM/our-planet',
     liveLink: 'https://hartiganhm.github.io/our-planet/',
-  }
+  },
+  // {
+  //   title: 'Our Planet',
+  //   description: 'A study in React, Redux, and Node.js centered around the well being of endangered animals across the globe',
+  //   techStack: 'React, Redux, Router, Node.js, Knex, PostgreSQL',
+  //   gitHub: 'https://github.com/HartiganHM/our-planet',
+  //   liveLink: 'https://hartiganhm.github.io/our-planet/',
+  // },
+  // {
+  //   title: 'Our Planet',
+  //   description: 'A study in React, Redux, and Node.js centered around the well being of endangered animals across the globe',
+  //   techStack: 'React, Redux, Router, Node.js, Knex, PostgreSQL',
+  //   gitHub: 'https://github.com/HartiganHM/our-planet',
+  //   liveLink: 'https://hartiganhm.github.io/our-planet/',
+  // }
 ]
