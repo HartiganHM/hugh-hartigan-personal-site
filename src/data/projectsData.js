95f48f5ab3f74d9913bbaf6c4af02f431f24244c
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-21 14:15:20 -0700

    Build projectsData data object

---
+++ src/data/projectsData.js
@@ -0,0 +1,9 @@
+export default [
+  {
+    title: 'Our Planet',
+    description: 'A study in React, Redux, and Node.js centered around the well being of endangered animals across the globe',
+    techStack: 'React, Redux, Router, Node.js, Knex, PostgreSQL',
+    gitHub: 'https://github.com/HartiganHM/our-planet',
+    liveLink: 'https://hartiganhm.github.io/our-planet/',
+  }
+]
