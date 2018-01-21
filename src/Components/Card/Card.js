b6fe2f81f225363a7ef31264bfe79b3beab6a3ca
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-21 16:45:13 -0700

    Build out Card component for Projects and complete styling

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -2,15 +2,30 @@ import React from 'react';
 import './Card.css';
 
 const Card = props => {
-  const { title, description, url, techStack, gitHub, liveLink } = props.data;
+  const {
+    image,
+    title,
+    description,
+    url,
+    techStack,
+    gitHub,
+    liveLink
+  } = props.data;
 
   if (gitHub) {
     return (
       <div className="Card">
-        <div className="wrapper">
+        <span className={image} />
+        <div className="content-container">
           <span className="title">{title}</span>
-          <span className="description">{description}</span>
-          <span className="tech-stack">{techStack}</span>
+          <div className="description-box">
+            <span className="description-header">Description</span>
+            <span className="description">{description}</span>
+          </div>
+          <div className="tech-stack-box">
+            <span className="tech-stack-header">Tech Stack</span>
+            <span className="tech-stack">{techStack}</span>
+          </div>
           <span className="button-wrapper">
             <a href={liveLink} className="button">
               Launch
