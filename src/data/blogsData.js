3f2b23514d2e6a27e36d74228b0ae3a79d7746f6
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-21 12:42:54 -0700

    Fix typo in blogsData

--- src/data/blogsData.js
+++ src/data/blogsData.js
@@ -1,6 +1,6 @@
 export default [
   {
-    title: 'No blogs current available',
+    title: 'No blogs currently available',
     description: 'Check back in the near future for updates!',
     url: ''
   }
