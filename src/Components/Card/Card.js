9c3638b5d77c8c5210b928f30e8e38a2d6b44ad8
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-21 15:03:44 -0700

    Add conditional rendering for Projects component

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import './Card.css';
 
-const Card = (props) => {
-  const { title, description, url } = props.data;
-  return (
-    <div className='Card'>
-      <a className='url' href={url}>
-        <span className='title'>{title}</span>
-        <span className='description'>{description}</span>
-      </a>
-    </div>
-  )
-}
+const Card = props => {
+  const { title, description, url, techStack, gitHub, liveLink } = props.data;
 
-export default Card
+  if (gitHub) {
+    return (
+      <div className="Card">
+        <div className="wrapper">
+          <span className="title">{title}</span>
+          <span className="description">{description}</span>
+          <span className="tech-stack">{techStack}</span>
+          <span className="button-wrapper">
+            <a href={liveLink} className="button">
+              Launch
+            </a>
+            <a href={gitHub} className="button">
+              GitHub
+            </a>
+          </span>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="Card">
+        <a className="url" href={url}>
+          <span className="title">{title}</span>
+          <span className="description">{description}</span>
+        </a>
+      </div>
+    );
+  }
+};
+
+export default Card;
