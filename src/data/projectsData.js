834e4865752fa939f5deaf8facccf2197984dc30
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-01-23 14:48:51 -0700

    Resrtucture projectData image properties

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,6 +1,6 @@
 export default [
   {
-    image: 'image our-planet-screen-shot',
+    image: 'our-planet-screen-shot',
     title: 'Our Planet',
     description: 'Our Planet is a web application built as part of the curriculum at Turing School of Software & Design. The project incorporates both a back-end database and a front-end user interface. The back-end repository was built using Node.js and Knex, while the front-end was constructed primarily through React and Redux. All styling was done through Sass and the testing suite runs through Enzyme and Jest.',
     techStack: 'React, Redux, Router, Node.js, Knex, PostgreSQL',
@@ -8,6 +8,7 @@ export default [
     liveLink: 'https://hartiganhm.github.io/our-planet/',
   },
   {
+    image: 'movie-tracker-screen-shot',
     title: 'Movie Tracker',
     description: 'A study in React, Redux, and Node.js centered around the well being of endangered animals across the globe',
     techStack: 'React, Redux, Router, Jest,Enzyme',
@@ -15,6 +16,7 @@ export default [
     liveLink: '',
   },
   {
+    image: 'swapi-box-screen-shot',
     title: 'SWAPI Box',
     description: 'Uses The Movie Database API to show the twenty most recently released movies. Allows users to log in with a username and password as well as save favorite movies.',
     techStack: 'React, SCSS, Jest, Enzyme',
