b3ffcd61e95b820a1463c8a8ed014bb3fb7a2bfd
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-04 19:06:30 -0700

    Add extra nav links to Header and adjust styling

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -5,14 +5,51 @@ import './About.css';
 const About = () => {
   return (
     <div className="About">
-    <div className="wrapper">
-          <span className="header-wordmark" />
+      <div className="wrapper">
+        <span className="header-wordmark" />
+        <span className="nav-bar">
           <Link className="nav-link" to="/">
             Home
           </Link>
+          <Link className="nav-link" to="/contact">
+            Contact
+          </Link>
+          <Link className="nav-link" to="/projects">
+            Projects
+          </Link>
+          <Link className="nav-link" to="/blogs">
+            Blogs
+          </Link>
+        </span>
+      </div>
+      <div className="info-container">
+        <img
+          className="profile-image"
+          src={require('../../images/Hugh-Hartigan.jpg')}
+          alt="Hugh-Hartigan-software-developer"
+        />
+        <div className="profile-description-container">
+          <p className="profile-description">
+            I am a software developer with a passion for creative solutions to
+            complex problems. I look forward to innovative challenges, relish in
+            the opportunity to learn something new, and love seeing those around
+            me grow.
+          </p>
+
+          <p className="profile-description">
+            My road to development started with an itch to create. Whether it's
+            painting, drawing, sewing a bow tie, or making a button pixel
+            perfect, I get lost building anything and everything. Software
+            development satisfies this need and more, providing me with an
+            endless ocean of new puzzles to solve.
+          </p>
+
+          <p className="profile-description">
+            Reach me through the contacts page of my site to collaborate, grab
+            coffee, or ponder life's biggest (and smallest) mysteries.
+          </p>
         </div>
-      <img className='profile-image' src={require('../../images/Hugh-Hartigan.jpg')} alt='Hugh-Hartigan-software-developer'/>
-      <Link className='nav-link' to="/">Home</Link>
+      </div>
     </div>
   );
 };
