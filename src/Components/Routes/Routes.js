224ea3b75aac315e7644bfd6627cc6292f06a57e
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-16 08:05:38 -0700

    Remove Contact route

--- src/Components/Routes/Routes.js
+++ src/Components/Routes/Routes.js
@@ -3,7 +3,6 @@ import { Route, Switch } from 'react-router-dom';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import LandingPage from '../LandingPage/LandingPage';
 import About from '../About/About';
-import Contact from '../Contact/Contact';
 import Projects from '../Projects/Projects';
 import Blogs from '../Blogs/Blogs';
 import './Routes.css';
@@ -23,7 +22,6 @@ class Routes extends Component {
             <Switch location={this.props.location}>
               <Route exact path="/about" render={() => <About />} />
               <Route exact path="/projects" render={() => <Projects />} />
-              <Route exact path="/contact" render={() => <Contact />} />
               <Route exact path="/blogs" render={() => <Blogs />} />
               <Route exact path="/" render={() => <LandingPage />} />
             </Switch>
