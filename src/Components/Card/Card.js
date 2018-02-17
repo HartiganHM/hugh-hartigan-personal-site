f1890862b27925fd3039798a4437dc7f16e2858a
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-17 09:43:11 -0700

    Build out blogs card and style

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -7,6 +7,7 @@ const Card = props => {
     title,
     description,
     url,
+    areaOfFocus,
     techStack,
     gitHub,
     liveLink
@@ -25,6 +26,10 @@ const Card = props => {
           <div className="description-box">
             <span className="description-header">Description</span>
             <span className="description">{description}</span>
+          </div>
+          <div className="area-of-focus-box">
+            <span className="area-of-focus-header">My Area of Focus</span>
+            <span className="area-of-focus">{areaOfFocus}</span>
           </div>
           <div className="tech-stack-box">
             <span className="tech-stack-header">Tech Stack</span>
@@ -46,15 +51,23 @@ const Card = props => {
   } else {
     return (
       <div className="Card">
-        <a className="url" href={url}>
-          <img
-            className="project-image"
-            src={require(`../../images/${image}.jpg`)}
-            alt={image}
-          />
-          <span className="title">{title}</span>
-          <span className="description">{description}</span>
-        </a>
+        <img
+          className="blog-image"
+          src={require(`../../images/${image}.jpg`)}
+          alt={image}
+        />
+        <span className="title">{title}</span>
+        <div className="content-container">
+          <div className="description-box">
+            <span className="description-header">Description</span>
+            <span className="description">{description}</span>
+          </div>
+          <span className="button-wrapper">
+            <a href={url} target="_blank" className="button">
+              Read
+            </a>
+          </span>
+        </div>
       </div>
     );
   }
