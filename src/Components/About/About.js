53238f39e0c20a9d9e48f49f3d422e0c9d32af33
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-10-15 14:49:03 -0600

    Style contact icons for mobile

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -36,58 +36,63 @@ const About = () => {
         </div>
 
         <div className="link-wrapper">
-          <a
-            href="https://www.turing.io/sites/default/files/resumes/Hugh-Hartigan-Resume.pdf"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="icon-wrapper"
-          >
-            <svg className="icon" viewBox="0 0 512 512">
-              <g>
-                <path d={svgPaths.resume} />
-              </g>
-            </svg>
-            <span className="icon-text">{about.iconText1}</span>
-          </a>
-          <a
-            href="https://github.com/HartiganHM"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="icon-wrapper"
-          >
-            <svg className="icon" viewBox="0 0 436.5 426.8">
-              <g>
-                <path d={svgPaths.github} />
-              </g>
-            </svg>
-            <span className="icon-text">{about.iconText2}</span>
-          </a>
-          <a
-            href="mailto:hartigan.hm@gmail.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="icon-wrapper"
-          >
-            <svg className="icon" viewBox="0 0 512 495.1">
-              <g>
-                <path d={svgPaths.email} />
-              </g>
-            </svg>
-            <span className="icon-text">{about.iconText3}</span>
-          </a>
-          <a
-            href="https://www.linkedin.com/in/hartiganhm/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="icon-wrapper"
-          >
-            <svg className="icon" viewBox="0 0 512 512">
-              <g>
-                <path d={svgPaths.linkedin} />
-              </g>
-            </svg>
-            <span className="icon-text">{about.iconText4}</span>
-          </a>
+          <div className="link-box">
+            <a
+              href="https://www.turing.io/sites/default/files/resumes/Hugh-Hartigan-Resume.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="icon-wrapper"
+            >
+              <svg className="icon" viewBox="0 0 512 512">
+                <g>
+                  <path d={svgPaths.resume} />
+                </g>
+              </svg>
+              <span className="icon-text">{about.iconText1}</span>
+            </a>
+            <a
+              href="https://github.com/HartiganHM"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="icon-wrapper"
+            >
+              <svg className="icon" viewBox="0 0 436.5 426.8">
+                <g>
+                  <path d={svgPaths.github} />
+                </g>
+              </svg>
+              <span className="icon-text">{about.iconText2}</span>
+            </a>
+          </div>
+
+          <div className="link-box">
+            <a
+              href="mailto:hartigan.hm@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="icon-wrapper"
+            >
+              <svg className="icon" viewBox="0 0 512 495.1">
+                <g>
+                  <path d={svgPaths.email} />
+                </g>
+              </svg>
+              <span className="icon-text">{about.iconText3}</span>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/hartiganhm/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="icon-wrapper"
+            >
+              <svg className="icon" viewBox="0 0 512 512">
+                <g>
+                  <path d={svgPaths.linkedin} />
+                </g>
+              </svg>
+              <span className="icon-text">{about.iconText4}</span>
+            </a>
+          </div>
         </div>
       </div>
     </div>
