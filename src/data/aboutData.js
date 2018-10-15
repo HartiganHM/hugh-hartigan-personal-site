5bb92d063b0cab9dc1b9d58e10733a17b269f618
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-10-15 12:14:10 -0600

    Build aboutData export for About page copy

---
+++ src/data/aboutData.js
@@ -0,0 +1,23 @@
+/*eslint-disable max-len*/
+
+export default {
+  header: 'About Hugh',
+  description1: `I am a software developer with a passion for creative solutions to
+  complex problems. I look forward to innovative challenges, relish
+  in the opportunity to learn something new, and love seeing those
+  around me grow.`,
+  description2: `My road to development started with an itch to create. Whether
+  it's painting, drawing, sewing a bow tie, or making a button pixel
+  perfect, I get lost building anything and everything. Software
+  development satisfies this need and more, providing me with an
+  endless ocean of new puzzles to solve.`,
+  description3: `Reach me through any of the links below to collaborate, grab
+  a coffee, or ponder life's biggest (and smallest) mysteries:`,
+  question1: 'Is Mario a really good hero or a very bad plumber?',
+  question2: 'If aliens invaded, would they think dogs owned us?',
+  question3: 'Is it possible to get the perfect temperature on a Hot Pocket?',
+  iconText1: 'Resume',
+  iconText2: 'GitHub',
+  iconText3: 'Email',
+  iconText4: 'LinkedIn'
+};
