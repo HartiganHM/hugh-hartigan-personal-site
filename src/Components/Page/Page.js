a5a4a837d2ba99a4a5d0c4391de1034289c26cee
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-10-15 12:42:38 -0600

    Remove Page component

--- src/Components/Page/Page.js
+++
@@ -1,6 +0,0 @@
-import React, { Component } from 'react';
-import Nav from '../Nav/Nav';
-import Card from '../Card/Card';
-import copyContent from '../../copy/copyContent';
-import PropTypes from 'prop-types';
-
