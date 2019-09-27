97977e0d15a30ff474941b5ce0804e73a51ef1ca
Hugh Hartigan <Hartigan.HM@gmail.com>
2019-09-27 07:44:07 -0600

    Refactor App to functional component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,59 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Routes from '../Routes/Routes';
 import { Route } from 'react-router';
 import classNames from 'classnames';
+
+import useMenuState from '../../hooks/useMenuState';
 import './App.scss';
 
-class App extends Component {
-  state = {
-    isResumeShown: false,
-    currentPage: 'home'
+const App = () => {
+  const currentPage = 'home';
+  const [isMenuShown] = useMenuState();
+
+  const appClasses = classNames({
+    App: currentPage === 'home',
+    'App away': currentPage !== 'home',
+    'no-scroll': isMenuShown
+  });
+
+  const componentProps = {
+    // onRedirect: this.handleRedirect
   };
 
-  componentDidMount() {
-    const currentPage = window.location.pathname.split('/')[1];
-
-    if (currentPage) {
-      this.setState({ currentPage });
-    }
-  }
-
-  handleRedirect = (event, page, push) => {
-    event.preventDefault();
-    const path = page === 'home' ? '/' : `/${page.toLowerCase()}`;
-
-    this.setState({ isMenuShown: false, currentPage: page });
-    setTimeout(() => push(path), 300);
-  };
-
-  render() {
-    const { isMenuShown, isResumeShown, currentPage } = this.state;
-
-    const appClasses = classNames({
-      App: currentPage === 'home',
-      'App away': currentPage !== 'home',
-      'no-scroll': isMenuShown || isResumeShown
-    });
-
-    const componentProps = {
-      onRedirect: this.handleRedirect
-    };
-
-    return (
-      <div className={appClasses}>
-        <Route
-          to="/"
-          render={({ location, history }) => (
-            <Routes
-              {...componentProps}
-              location={location}
-              history={history}
-            />
-          )}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className={appClasses}>
+      <Route
+        to="/"
+        render={({ location, history }) => (
+          <Routes {...componentProps} location={location} history={history} />
+        )}
+      />
+    </div>
+  );
+};
 
 export default App;
