3c2cf1c8ab2d320f88106efe1d730f3fc32d79a2
Hugh Hartigan <Hartigan.HM@gmail.com>
2019-09-27 06:16:20 -0600

    Refactor Blogs to functional component

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -1,33 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+
 import Nav from '../Nav/Nav';
 import Card from '../Card/Card';
 import copyContent from '../../copy/copyContent';
-import PropTypes from 'prop-types';
 import './Blogs.scss';
 
-class Blogs extends Component {
-  render() {
-    const {
-      toggleMenu,
-      toggleDetails,
-      onRedirect,
-      isMenuShown,
-      history
-    } = this.props;
-    const navProps = { toggleMenu, onRedirect, isMenuShown, history };
+const Blogs = ({
+  toggleMenu,
+  toggleDetails,
+  onRedirect,
+  isMenuShown,
+  history
+}) => {
+  const navProps = { toggleMenu, onRedirect, isMenuShown, history };
 
-    const blogPosts = copyContent.blogs.map((blog, index) => (
-      <Card key={index} cardData={blog} toggleDetails={toggleDetails} />
-    ));
+  const blogPosts = copyContent.blogs.map((blog, index) => (
+    <Card key={index} cardData={blog} toggleDetails={toggleDetails} />
+  ));
 
-    return (
-      <div className="Blogs">
-        <Nav currentPage="Blogs" {...navProps} />
-        <div className="card-container">{blogPosts}</div>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="Blogs">
+      <Nav currentPage="Blogs" {...navProps} />
+      <div className="card-container">{blogPosts}</div>
+    </div>
+  );
+};
 
 export default Blogs;
 
