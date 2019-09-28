d47d799999ef4524675c8a20434be47c4304c2e4
Hugh Hartigan <Hartigan.HM@gmail.com>
2019-09-28 09:57:01 -0600

    Build index.js for components

---
+++ src/components/index.js
@@ -0,0 +1,21 @@
+import About from './About/About';
+import App from './App/App';
+import Blogs from './Blogs/Blogs';
+import Card from './Card/Card';
+import LandingPage from './LandingPage/LandingPage';
+import Loader from './Loader/Loader';
+import Nav from './Nav/Nav';
+import Projects from './Projects/Projects';
+import { StateProvider } from './StateProvider/StateProvider';
+
+export {
+  About,
+  App,
+  Blogs,
+  Card,
+  LandingPage,
+  Loader,
+  Nav,
+  Projects,
+  StateProvider
+};
