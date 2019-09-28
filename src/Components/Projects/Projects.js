e0750113a3e09428c866313fbc8427f3ca076602
Hugh Hartigan <Hartigan.HM@gmail.com>
2019-09-28 10:33:15 -0600

    Remove Nav from Projects

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,30 +1,19 @@
 import React from 'react';
-import { object } from 'prop-types';
 
-import Nav from '../Nav/Nav';
 import Card from '../Card/Card';
 import copyContent from '../../copy/copyContent';
 import './Projects.scss';
 
-const Projects = ({
-  history
-}) => {
-  const navProps = { history };
-
+const Projects = () => {
   const projects = copyContent.projects.map((project, index) => (
     <Card key={index} cardData={project} />
   ));
 
   return (
     <div className="Projects">
-      <Nav currentPage="Projects" {...navProps} />
       <div className="card-container">{projects}</div>
     </div>
   );
 };
 
 export default Projects;
-
-Projects.propTypes = {
-  history: object
-};
