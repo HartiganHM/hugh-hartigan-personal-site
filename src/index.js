1cf32a3eb5ef4a58c41383086286dccab35c9852
Hugh Hartigan <Hartigan.HM@gmail.com>
2019-09-28 09:57:22 -0600

    Add StateProvider to src/index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import App from './Components/App/App';
+
+import { App, StateProvider } from './components';
 import './index.css';
+
+const initialState = {
+  currentPage: 'home',
+  isMenuShown: false
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+  case 'CHANGE_CURRENT_PAGE': {
+    return {
+      ...state,
+      currentPage: action.currentPage
+    };
+  }
+  case 'TOGGLE_MENU': {
+    return {
+      ...state,
+      isMenuShown: action.isMenuShown
+    };
+  }
+  default:
+    return state;
+  }
+};
 
 const router = (
   <BrowserRouter>
-    <App />
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
   </BrowserRouter>
 );
 
